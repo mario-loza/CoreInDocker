41dc6c1d10db98f5c11ec482e8135f8fcbcf8f8f
Mario Alejandro Loza Armand Ugon <mario.loza@gmail.com>
2020-06-09 16:05:05 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CoreInDocker
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine($" Hello Human, I am running on : {Environment.OSVersion}");
+      Console.WriteLine($" in the machine : {Environment.MachineName}");
+      Console.WriteLine($" ,and my current folder is : {Environment.CurrentDirectory} !");
+    }
+  }
+}
